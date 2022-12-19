ce8d34d75b7ec2e34afdc5d2cecfc3272f8aa7ea
= <=>
2022-12-19 11:54:39 +0300

    Refactor

--- kernel/halving.php
+++ kernel/halving.php
@@ -31,11 +31,9 @@ $price = $halving->getPrice($bot->getOrderbook($symbol));
 $grid_buys = $halving->getGridBuy($grid, $price);
 $count_real_orders_buy = count($grid_buys);
 if ($count_real_orders_buy > 0) {
-    $balance_total = $balances[$quote_asset]['total'];
-    $deal_amount_buy = $halving->getDealAmountBuy($balance_total, $count_real_orders_buy, $price);
     $open_order_buys = $halving->getOpenOrderBuys($open_orders);
-
     $need_cancel_order_buys = $halving->getNeedCancelOrderBuys($open_order_buys, $grid_buys);
+
     foreach ($need_cancel_order_buys as $need_cancel_order_buy) {
         $bot->cancelOrder($need_cancel_order_buy['id'], $need_cancel_order_buy['symbol']);
         echo '[' . date('Y-m-d H:i:s') . '] [BUY] Cancel order: ' . $need_cancel_order_buy['id'] . ', ' . $need_cancel_order_buy['price'] . PHP_EOL;
@@ -44,9 +42,6 @@ if ($count_real_orders_buy > 0) {
     $balances = $bot->getBalances([$base_asset, $quote_asset]);
     $open_orders = $bot->getOpenOrders($symbol);
 
-    $balance_total = $balances[$quote_asset]['total'];
-    $count_real_orders_buy = count($grid_buys);
-    $deal_amount_buy = $halving->getDealAmountBuy($balance_total, $count_real_orders_buy, $price);
     $open_order_buys = $halving->getOpenOrderBuys($open_orders);
     $grid_status_buys = $halving->getGridStatusBuy($grid_buys, $open_order_buys, $count_real_orders_buy);
 
@@ -55,6 +50,7 @@ if ($count_real_orders_buy > 0) {
         echo '[' . date('Y-m-d H:i:s') . '] [BUY] Cancel order: ' . $need_cancel['id'] . PHP_EOL;
     }
     foreach ($halving->needCreate($grid_status_buys) as $need_create) {
+        $deal_amount_buy = $halving->getDealAmountBuy($balances[$quote_asset]['total'], $count_real_orders_buy, $price);
         $bot->createOrder($symbol, 'limit', 'buy', $deal_amount_buy, $need_create['price']);
         echo '[' . date('Y-m-d H:i:s') . '] [BUY] Create order: ' . $need_create['price'] . ', ' . $deal_amount_buy . PHP_EOL;
     }
@@ -65,11 +61,9 @@ if ($count_real_orders_buy > 0) {
 $grid_sells = $halving->getGridSell($grid, $price);
 $count_real_orders_sell = count($grid_sells);
 if ($count_real_orders_sell > 0) {
-    $balance_total = $balances[$base_asset]['total'];
-    $deal_amount_sell = $halving->getDealAmountSell($balance_total, $count_real_orders_sell);
     $open_order_sells = $halving->getOpenOrderSells($open_orders);
-
     $need_cancel_order_sells = $halving->getNeedCancelOrderSells($open_order_sells, $grid_sells);
+
     foreach ($need_cancel_order_sells as $need_cancel_order_sell) {
         $bot->cancelOrder($need_cancel_order_sell['id'], $need_cancel_order_sell['symbol']);
         echo '[' . date('Y-m-d H:i:s') . '] [SELL] Cancel order: ' . $need_cancel_order_sell['id'] . ', ' . $need_cancel_order_sell['price'] . PHP_EOL;
@@ -78,9 +72,6 @@ if ($count_real_orders_sell > 0) {
     $balances = $bot->getBalances([$base_asset, $quote_asset]);
     $open_orders = $bot->getOpenOrders($symbol);
 
-    $balance_total = $balances[$base_asset]['total'];
-    $count_real_orders_sell = count($grid_sells);
-    $deal_amount_sell = $halving->getDealAmountSell($balance_total, $count_real_orders_sell);
     $open_order_sells = $halving->getOpenOrderSells($open_orders);
     $grid_status_sells = $halving->getGridStatusSell($grid_sells, $open_order_sells, $count_real_orders_sell);
 
@@ -89,6 +80,7 @@ if ($count_real_orders_sell > 0) {
         echo '[' . date('Y-m-d H:i:s') . '] [SELL] Cancel order: ' . $need_cancel['id'] . PHP_EOL;
     }
     foreach ($halving->needCreate($grid_status_sells) as $need_create) {
+        $deal_amount_sell = $halving->getDealAmountSell($balances[$base_asset]['total'], $count_real_orders_sell);
         $bot->createOrder($symbol, 'limit', 'sell', $deal_amount_sell, $need_create['price']);
         echo '[' . date('Y-m-d H:i:s') . '] [SELL] Create order: ' . $need_create['price'] . ', ' . $deal_amount_sell . PHP_EOL;
     }
